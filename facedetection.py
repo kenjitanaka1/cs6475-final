3ccd854649b4278183339ccac9667058febba495
Kenji Tanaka <kenjit.tanaka3@gmail.com>
2019-04-26 12:46:21 -0400

    more cleanup

--- facedetection.py
+++ facedetection.py
@@ -1,17 +1,10 @@
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.svm import SVC
-from sklearn.datasets import fetch_lfw_people
-from sklearn.utils import shuffle
 import os
 import cv2
 import random
 import math
 import numpy as np
 import cyvlfeat as vlfeat
-from joblib import dump, load
 from scipy import ndimage
-from sklearn.cluster import AgglomerativeClustering
-import sklearn.feature_extraction
 import time
 from skimage.morphology import watershed
 from skimage.feature import peak_local_max
@@ -29,47 +22,6 @@ classifier = AdaBoostClassifier(n_estimators=200,algorithm='SAMME.R', random_sta
 # http://www.robots.ox.ac.uk/~vgg/data3.html
 # http://ijettjournal.org/2016/volume-31/number-4/IJETT-V31P236.pdf
 
-def get_img_paths(path):
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.jpg' in file or '.JPG' in file:
-                files.append(os.path.join(r, file))
-    return files
-
-def trainDetector():
-    # # load data
-    positivePath = 'data/lfw'
-    negativePaths = 'data/HomeObject06Test'
-    facePaths = get_img_paths(positivePath)
-    negativePaths = get_img_paths(negativePaths)
-    faceFeats = []
-    cell_size = 6
-    for path in facePaths:
-        img = cv2.resize(cv2.imread(path),(96,96))
-        faceFeats.append(np.reshape(vlfeat.hog.hog(img, cell_size),-1))
-
-    numNegs = math.ceil(len(faceFeats) / len(negativePaths)) * 2
-    frameSize = (96,96,3)
-    negFeats = []
-    for path in negativePaths:
-        negImg = cv2.imread(path)
-        pts = np.vstack((np.random.randint(negImg.shape[0]-frameSize[0],size=numNegs),
-            np.random.randint(negImg.shape[1]-frameSize[1],size=numNegs))).T
-        for pt in pts: # select random point
-            imgSlice = negImg[pt[0]:pt[0]+frameSize[0], pt[1]:pt[1]+frameSize[1],:]
-            negFeats.append(np.reshape(vlfeat.hog.hog(imgSlice,cell_size),-1))
-        # extract
-
-    X = faceFeats + negFeats
-    y = [True] * len(faceFeats) + [False] * len(negFeats)
-    X, y = shuffle(list(X), y, random_state=777)
-
-    print('Dataset size: {}'.format(len(X)))
-    classifier.fit(X, y)
-    print('Saving model to model.joblib')
-    dump(classifier, 'model.joblib') 
 # src : https://stackoverflow.com/questions/34325879/how-to-efficiently-find-clusters-of-like-elements-in-a-multidimensional-array 
 def find_clusters(array):
     clustered = np.empty_like(array)
